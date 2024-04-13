c1a71511d2f2e7c4c34d3b94aefe17e6c83504fd
joaodibba <joaomiranda.pessoal@gmail.com>
2024-04-13 13:49:08 +0100

    updt

--- CPP_01/ex05/Harl.hpp
+++ CPP_01/ex05/Harl.hpp
@@ -6,15 +6,17 @@
 
 class Harl
 {
-public:
-	Harl();
-	~Harl();
-	void 	complain(std::string level);
+
 private:
 	void		_debug();
 	void		_info();
 	void		_warning();
 	void		_error();
+
+public:
+	Harl();
+	~Harl();
+	void 	complain(std::string level);
 };
 
 #endif
