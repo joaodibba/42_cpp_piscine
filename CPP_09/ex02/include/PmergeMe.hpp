1ff09f69bad88087ccd829fa013cd4cb78c82ed9
joaodibba <joaomiranda.pessoal@gmail.com>
2025-03-29 19:06:56 +0000

    pls end

--- CPP_09/ex02/include/PmergeMe.hpp
+++ CPP_09/ex02/include/PmergeMe.hpp
@@ -2,19 +2,137 @@
 #define PMERGEME_HPP
 
 #include <vector>
+#include <list>
+#include <deque>
 #include <iostream>
 #include <algorithm>
 
+template <typename T,
+          template <typename U, typename Alloc = std::allocator<U> > class Container>
 class PmergeMe
 {
 private:
     PmergeMe();
     ~PmergeMe();
-    PmergeMe(const PmergeMe &other);
-    PmergeMe &operator=(const PmergeMe &other);
+    PmergeMe(const PmergeMe &);
+    PmergeMe &operator=(const PmergeMe &);
 
 public:
-    static std::vector<int> sort(const std::vector<int> &arr);
+    static Container<T> sort(const Container<T> &arr);
 };
+template <typename T, template <typename U, typename Alloc = std::allocator<U> > class Container>
+struct PairContainer
+{
+    Container<std::pair<T, T> > pairs;
+    T unpaired;
+    bool unpaired_found;
+
+    PairContainer() : unpaired_found(false) {}
+};
+
+template <typename T, template <typename U, typename Alloc = std::allocator<U> > class Container>
+struct MinMaxContainer
+{
+    Container<T> mins;
+    Container<T> maxs;
+};
+
+template <typename T, template <typename U, typename Alloc = std::allocator<U> > class Container>
+PairContainer<T, Container> make_pairs_with_unpaired(const Container<T> &arr)
+{
+    PairContainer<T, Container> result;
+
+    typename Container<T>::const_iterator it = arr.begin();
+    typename Container<T>::const_iterator end = arr.end();
+
+    while (it != end)
+    {
+        T first = *it;
+        ++it;
+        if (it == end)
+        {
+            // We have an unpaired leftover
+            result.unpaired = first;
+            result.unpaired_found = true;
+            break;
+        }
+        T second = *it;
+        ++it;
+        result.pairs.push_back(std::make_pair(first, second));
+    }
+    return result;
+}
+
+template <typename T, template <typename U, typename Alloc = std::allocator<U> > class Container>
+MinMaxContainer<T, Container>
+split_mins_and_maxs(const Container<std::pair<T, T> > &pairs)
+{
+    MinMaxContainer<T, Container> mm;
+
+    typename Container<std::pair<T, T> >::const_iterator it = pairs.begin();
+    typename Container<std::pair<T, T> >::const_iterator end = pairs.end();
+
+    for (; it != end; ++it)
+    {
+        if (it->first < it->second)
+        {
+            mm.mins.push_back(it->first);
+            mm.maxs.push_back(it->second);
+        }
+        else
+        {
+            mm.mins.push_back(it->second);
+            mm.maxs.push_back(it->first);
+        }
+    }
+    return mm;
+}
+
+template <typename T, template <typename U, typename Alloc = std::allocator<U> > class Container>
+void sorted_insert(Container<T> &sorted, const T &value)
+{
+    typename Container<T>::iterator it = sorted.begin();
+    typename Container<T>::iterator ed = sorted.end();
+
+    for (; it != ed; ++it)
+    {
+        if (value < *it)
+        {
+            sorted.insert(it, value);
+            return;
+        }
+    }
+    sorted.push_back(value);
+}
+
+template <typename T, template <typename U, typename Alloc = std::allocator<U> > class Container>
+void insert_mins(Container<T> &sorted, const Container<T> &mins)
+{
+    typename Container<T>::const_iterator it = mins.begin();
+    typename Container<T>::const_iterator end = mins.end();
+
+    for (; it != end; ++it)
+    {
+        sorted_insert(sorted, *it);
+    }
+}
+
+template <typename T, template <typename U, typename Alloc = std::allocator<U> > class Container>
+Container<T> PmergeMe<T, Container>::sort(const Container<T> &arr)
+{
+    if (arr.size() <= 1)
+        return arr;
+
+    PairContainer<T, Container> pc = make_pairs_with_unpaired<T, Container>(arr);
+    MinMaxContainer<T, Container> mm = split_mins_and_maxs<T, Container>(pc.pairs);
+
+    Container<T> sorted_maxs = PmergeMe<T, Container>::sort(mm.maxs);
+    insert_mins(sorted_maxs, mm.mins);
+
+    if (pc.unpaired_found)
+        sorted_insert(sorted_maxs, pc.unpaired);
+
+    return sorted_maxs;
+}
 
 #endif
--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -1,21 +1,22 @@
 #include "../include/PmergeMe.hpp"
-
 #include <iostream>
 #include <vector>
-#include <cstdlib>
+#include <list>
 #include <climits>
-#include <cstring>
+#include <cstdlib>
 #include <cctype>
+#include <sys/time.h>
+#include <iomanip>
 
 bool isValidNumber(const char *str)
 {
-    if (!str || !*str)
+    if (str == NULL || *str == '\0')
         return false;
 
     if (*str == '+' || *str == '-')
         ++str;
 
-    if (!*str)
+    if (*str == '\0')
         return false;
 
     while (*str)
@@ -24,48 +25,76 @@ bool isValidNumber(const char *str)
             return false;
         ++str;
     }
-
     return true;
+}
+
+double getTimeSeconds()
+{
+    struct timeval tv;
+    gettimeofday(&tv, 0);
+    return (double)tv.tv_sec + (double)tv.tv_usec * 1.0e-6;
 }
 
 int main(int ac, char **av)
 {
     if (ac < 2)
     {
-        std::cerr << "Usage: ./PmergeMe <integers to sort>" << std::endl;
+        std::cerr << "Usage: " << av[0] << " <integers to sort>\n";
         return 1;
     }
 
-    std::vector<int> input;
+    std::vector<int> vec;
+    std::list<int>   lst;
 
     for (int i = 1; i < ac; ++i)
     {
         if (!isValidNumber(av[i]))
         {
-            std::cerr << "Error: Invalid input. Please enter valid integers." << std::endl;
+            std::cerr << "Error: Invalid input '" << av[i]
+                      << "'; please enter only positive integers.\n";
             return 1;
         }
 
         long num = std::atol(av[i]);
         if (num < 1 || num > INT_MAX)
         {
-            std::cerr << "Error: Integer out of range." << std::endl;
+            std::cerr << "Error: Integer out of range.\n";
             return 1;
         }
 
-        input.push_back(static_cast<int>(num));
+        vec.push_back(static_cast<int>(num));
+        lst.push_back(static_cast<int>(num));
     }
 
-    std::vector<int> sorted = PmergeMe::sort(input);
-
-    for (std::size_t i = 0; i < sorted.size(); ++i)
-    {
-        std::cout << sorted[i];
-        if (i + 1 < sorted.size())
-            std::cout << " ";
-    }
-
+    std::cout << "Before: ";
+    for (int i = 1; i < ac; ++i)
+        std::cout << av[i] << " ";
     std::cout << std::endl;
+
+    double startVec = getTimeSeconds();
+    std::vector<int> vecSorted = PmergeMe<int, std::vector>::sort(vec);
+    double endVec = getTimeSeconds();
+
+    std::cout << "After:  ";
+    for (std::vector<int>::size_type i = 0; i < vecSorted.size(); ++i)
+        std::cout << vecSorted[i] << " ";
+    std::cout << std::endl;
+
+    double timeVecUs = (endVec - startVec) * 1.0e6;
+
+    double startList = getTimeSeconds();
+    std::list<int> lstSorted = PmergeMe<int, std::list>::sort(lst);
+    double endList = getTimeSeconds();
+
+    double timeListUs = (endList - startList) * 1.0e6;
+
+    std::cout << "Time to process a range of " << vec.size()
+              << " elements with std::vector : " 
+              << std::fixed << std::setprecision(5) << timeVecUs << " us" << std::endl;
+
+    std::cout << "Time to process a range of " << lst.size()
+              << " elements with std::list   : " 
+              << std::fixed << std::setprecision(5) << timeListUs << " us" << std::endl;
 
     return 0;
 }
